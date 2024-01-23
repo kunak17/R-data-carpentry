c39503269b4407c04147d6c33dc4cb951d60c57c
Karolina Kuna <karolina@Karolinas-MacBook-Pro.local>
2024-01-23 16:13:53 +0100

    Deletion also needs to be commited

--- Second script.R
+++
@@ -1,6 +0,0 @@
-#This is a test file, where I created a new version control project control from
-#scratch to see if stuff works the way I expect it too
-#First I pull
-#Then save
-#Then commit
-#Then push - so the post savr comments shouldn't exist
